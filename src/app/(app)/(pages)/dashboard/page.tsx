6d4eb9258f443bfeb3e8a5a76d7ccb66b35f3981
figodevtech <figo.devtech@gmail.com>
2025-11-03 16:48:53 -0300

    att

--- src/app/(app)/(pages)/dashboard/page.tsx
+++ src/app/(app)/(pages)/dashboard/page.tsx
@@ -8,16 +8,14 @@ import FinancialContent from "./components/financialContent";
 export default function Dashboard() {
   return (
     <div className="w-full">
-      <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
-        <h1 className="text-2xl font-bold text-foreground">Dashboard</h1>
-      </div>
-      <Separator className="my-4" />
+      
 
       <div className="flex gap-4">
-        <Tabs defaultValue="geral" className="">
+        <Tabs defaultValue="geral" className="w-full">
           <TabsList className="bg-none gap-2">
             <TabsOptions />
           </TabsList>
+      <Separator className="my-4" />
 
 
           <TabsContent value="geral">
