b1cb8a5443b708e9165b7ba0381221b2a23e729f
Bruno Lima <brunoblima1@gmail.com>
2025-11-14 10:08:29 -0300

    ajuste no redirecionamento quando logado

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,10 +1,17 @@
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import { LoginForm } from "./components/login-form";
 import { getQuoteOfTheDay } from "@/lib/quotes";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function LoginPage() {
   const { text, author, obs } = getQuoteOfTheDay();
+  const session = await auth();
 
+  if (session) {
+    redirect("/dashboard");
+  }
+  
   return (
     <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <span className="mb-4 text-xs text-muted-foreground">
