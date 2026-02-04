6c0ad82493f6b919d3ce60c11cc38c6803b656a1
figodevtech <figo.devtech@gmail.com>
2026-02-04 12:21:40 -0300

    att fornecedor

--- src/app/api/tipos/fornecedores/route.ts
+++ src/app/api/tipos/fornecedores/route.ts
@@ -59,7 +59,7 @@ export async function GET(req: Request) {
       `,
         { count: "exact" }
       )
-      .order("nomerazaosocial", { ascending: true });
+      .order("id", { ascending: false });
 
     // filtro de busca
     if (search) {
